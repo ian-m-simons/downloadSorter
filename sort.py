0d9e578f95db1ea19579ecb86a7eda9af79ecb32
Ian Simons <iansimons93@gmail.com>
2024-11-13 12:27:20 -0500

    change way paths are generated

--- sort.py
+++ sort.py
@@ -2,18 +2,28 @@ import os
 import platform
 
 
-Path = ""
+print("success")
+Paths = {"Downloads":"", "Documents":"", "Pictures":"", "Music":"", "Videos":""}
+PathList = list(Paths.keys())
+OperatingSystem = platform.system()
 username = os.environ.get('USER', os.environ.get('USERNAME'))
-if platform.system() == "Windows":
-    Path = "C:\\Users\\"+username+"\\Downloads"
+if OperatingSystem == "Windows":
+    tempPath = os.path.join("C:/Users/"+username) 
+    for i in range(len(PathList)):
+        Paths[PathList[i]]= tempPath + "/" + PathList[i]
+        
 
 else:
-    Path = "/home/"+username+"/Downloads"
+    tempPath = "/home/"+username
+    for i in range(len(PathList)):
+        Paths[PathList[i]] = tempPath + "/"+ PathList[i]
+print(Paths)
 
-downloadContents = os.listdir(Path)
-
+downloadContents = os.listdir(Paths['Downloads'])
+"""
 for i in range(len(downloadContents)):
     extension = downloadContents[i].rsplit(".", 1)
     if (extension == ".png") or (extension == ".jpg"):
-        if platform.system() == "Windows":
+        if OperatingSystem == "Windows":
             
+"""         
